8545a400e1054910188b296fc661a09184af04c1
Autumn Stephens <aust8150@colorado.edu>
2026-06-22 10:56:17 -0600

    Adjusted values of 'np.arange'

--- src/sample_code.py
+++ src/sample_code.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x_values = np.arange(0, 10, 1)
+# Changed end arguments of 'np.arange'
+x_values = np.arange(0, 1, 0.1)
 y_values = x_values**2
 
 plt.plot(x_values, y_values)
