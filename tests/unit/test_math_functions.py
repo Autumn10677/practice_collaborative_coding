cc6196bfd18933b3a040f02344ba2bbf2bdbf020
Autumn Stephens <aust8150@colorado.edu>
2026-06-23 12:52:56 -0600

    Added unit test for 'add' function

---
+++ tests/unit/test_math_functions.py
@@ -0,0 +1,26 @@
+import sys
+import unittest
+import numpy as np
+
+sys.path.append("src/")  # noqa
+
+import math_utils
+
+class TestExtractionFunctions(unittest.TestCase):
+
+    def test_add(self):
+
+        # Done many times with rand. numbers to test robustness
+        for _ in range(100):
+
+            # Generates two random numbers bewteen 0 and 1
+            x, y = np.random.uniform(0, 1, 2)
+
+            # This should always hold!
+            self.assertTrue(
+                math_utils.add(x, y) == x + y
+            )
+
+
+if __name__ == "__main__":
+    unittest.main()
