61a12a01a170abdd35607771a77c008c0178a9d8
Autumn Stephens <aust8150@colorado.edu>
2026-07-07 10:01:18 -0600

    Fixed formatting of unit test script

--- tests/unit/test_math_functions.py
+++ tests/unit/test_math_functions.py
@@ -6,9 +6,12 @@ sys.path.append("src/")  # noqa
 
 import math_utils
 
+
 class TestExtractionFunctions(unittest.TestCase):
 
     def test_add(self):
+
+        self.assertTrue(False)
 
         # Done many times with rand. numbers to test robustness
         for _ in range(100):
@@ -17,9 +20,7 @@ class TestExtractionFunctions(unittest.TestCase):
             x, y = np.random.uniform(0, 1, 2)
 
             # This should always hold!
-            self.assertTrue(
-                math_utils.add(x, y) == x + y
-            )
+            self.assertTrue(math_utils.add(x, y) == x + y)
 
 
 if __name__ == "__main__":
